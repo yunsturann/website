8c5f7204d457446260fb6886ef406ddd142183d6
Yunus <101173844+yunsturann@users.noreply.github.com>
2022-08-07 13:43:34 +0300

    main website changes
    hide navbar on scroll

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// hide navbar on scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
