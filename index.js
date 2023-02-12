c54213f40069ca007f77ae809f0c846b829813dc
Yunus <ynstrn811@gmail.com>
2023-02-13 02:00:45 +0300

    some small changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const rightSide = document.querySelector(".right-side");
+const navToggle = document.querySelector(".nav-toggle");
+const navbar = document.getElementById("navbar");
 
 // hide navbar on scroll
 
@@ -5,22 +8,24 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    navbar.style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-130px";
+    navbar.style.top = "-130px";
+    if(window.getComputedStyle(navToggle).display === "block"){
+      rightSide.setAttribute("style","display:none");
+    }
   }
   prevScrollpos = currentScrollPos;
 }
 
 let screen = window.matchMedia("(max-width: 600px)");
-const rightSide = document.querySelector(".right-side");
-const navToggle = document.querySelector(".nav-toggle");
 
 screen.addListener(myFunction); // listen screen width
 navToggle.addEventListener("click",renderNavbar); // nav toggle click event
 
 function myFunction(screen){
   if (screen.matches) { // 600px or less
+    //navToggle.style.display === "block";
     rightSide.setAttribute("style","display:none");
   }
   else{ //more than 600px
