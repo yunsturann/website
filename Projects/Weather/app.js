a5076eb9df0eb3b34a75846ad56bc5c75352ef2b
Yunus <ynstrn811@gmail.com>
2023-02-20 19:02:21 +0300

    time and units are added

--- Projects/Weather/app.js
+++ Projects/Weather/app.js
@@ -5,7 +5,11 @@ let weather = {
     icon: "",
     description: "",
     humidity:"" ,
-    wind:""
+    wind:"",
+    unit :{
+        speed:"",
+        temperature:""
+    }
 };
 
 // when document is ready, get default-city from local storage, then set and display
@@ -27,6 +31,9 @@ $("#search-input").keypress((e)=>{
 //click event
 $("#submit").click(submitCity);
 
+//event for radioes call setWeather function with city parameter 
+$(".form-check-input").click(()=>setWeather(weather.city));
+
 function submitCity(){
     setWeather($("#search-input").val());
     //clear value 
@@ -37,8 +44,10 @@ function submitCity(){
 function setWeather(city){
     // set city, then get values from API with respect to the city.
     weather.city = city;
-    const url ="https://api.openweathermap.org/data/2.5/weather?q=" + weather.city + "&appid=5e5043"+ weather.userId +"452e5b&units=metric";
+    let unit = $(".form-check-input:checked").val();
+    const url ="https://api.openweathermap.org/data/2.5/weather?q=" + weather.city + "&appid=5e5043"+ weather.userId +"452e5b&units="+ unit;
     fetch(url).then(res=>{
+        console.log(res);
         if(res.ok){
             return res.json();
         }else {
@@ -51,6 +60,13 @@ function setWeather(city){
         weather.description = data.weather[0].description;
         weather.humidity = data.main.humidity;
         weather.wind = data.wind.speed;
+        if(unit === "metric"){
+            weather.unit.speed = "meter/sec";
+            weather.unit.temperature = "celcius";
+        }else{
+            weather.unit.speed = "miles/hour";
+            weather.unit.temperature = "fahrenheit";
+        }
         displayWeather();
     }).catch((err)=>alert(err));
 }
@@ -59,12 +75,23 @@ function setWeather(city){
 function displayWeather(){
     $("#city").text(`Weather in ${weather.city}`);
     $("body").css("background-image","url('https://source.unsplash.com/1600x900/?" + weather.city + "')");
-    $("#temp").text(weather.temp +"° Celcius");
+    $("#temp").text(weather.temp +"° "+weather.unit.temperature);
     $("#weathor-icon").attr("src","http://openweathermap.org/img/wn/"+ weather.icon +"@2x.png")
     $("#weather-description").text(weather.description);
     $(".humidity").text(`Humidity: ${weather.humidity}%`)
-    $(".wind").text("Wind speed: " + weather.wind +" km/h");
+    $(".wind").text("Wind speed: " + weather.wind +" "+ weather.unit.speed);
     
     // set current city as a default 
     localStorage.setItem("default-city",weather.city);
 }
+
+/* Time Section*/
+
+function time(){
+    let localTime = new Date().toLocaleTimeString(); // get local time as a string
+    $("#time").text(localTime); // set value of time
+}
+
+setInterval(time,1000); // call every second
+
+
