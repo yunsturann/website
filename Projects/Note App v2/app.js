fed481174f39cb726075820ede9e7bec3f79540f
Yunus <ynstrn811@gmail.com>
2023-08-17 04:01:25 +0300

    note app v2 is almost done

--- Projects/Note App v2/app.js
+++ Projects/Note App v2/app.js
@@ -1,5 +1,6 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
+const popupTitle = popupBox.querySelector("header p");
 const closeIcon = document.getElementById("close-icon");
 const btnAdd = document.getElementById("btn-add");
 
@@ -23,20 +24,80 @@ const months = [
 //getting localstorage notes if exist and parsing them
 // to js object else passing an empty array to notes
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpDate = false,
+  updateId;
 
 addBox.addEventListener("click", () => {
+  titleInput.focus();
   popupBox.classList.add("show");
 });
 
 closeIcon.addEventListener("click", () => {
+  btnAdd.innerText = "Add Note";
+  popupTitle.innerText = "Add a new Note";
   popupBox.classList.remove("show");
 });
+
+function showNotes() {
+  document.querySelectorAll(".note").forEach((note) => note.remove());
+  notes.forEach((note, index) => {
+    let liTag = ` <li class="note">
+        <div class="details">
+          <p>${note.title}</p>
+          <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+          <span>${note.date}</span>
+          <div class="settings">
+            <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+            <ul class="menu">
+              <li onclick="updateNote(${index}, '${note.title}', '${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+              <li onclick="deleteNote(${index})"><i  class="uil uil-trash"></i>Delete</li>
+            </ul>
+          </div>
+        </div>
+      </li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
+}
+showNotes();
+
+function showMenu(elem) {
+  elem.parentElement.classList.add("show");
+
+  document.addEventListener("click", (e) => {
+    //removing show class from the settings menu on document click
+    if (e.target.tagName != "I" || e.target != elem) {
+      elem.parentElement.classList.remove("show");
+    }
+  });
+}
+
+function deleteNote(index) {
+  if (!confirm("Are you sure to delete the note ?")) return;
+  notes.splice(index, 1);
+  //saving notes to localstorage
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+}
+
+function updateNote(index, title, desc) {
+  addBox.click();
+  isUpDate = true;
+  updateId = index;
+  titleInput.value = title;
+  descInput.value = desc;
+  btnAdd.innerText = "Update Note";
+  popupTitle.innerText = "Update a Note";
+}
 
 btnAdd.addEventListener("click", (e) => {
   e.preventDefault();
 
   let noteTitle = titleInput.value;
   let noteDesc = descInput.value;
+  titleInput.value = "";
+  descInput.value = "";
 
   if (noteTitle || noteDesc) {
     let dateObj = new Date(),
@@ -49,9 +110,17 @@ btnAdd.addEventListener("click", (e) => {
       description: noteDesc,
       date: `${months[month]} ${day}, ${year}`,
     };
-    notes.push(noteInfo);
+
+    if (!isUpDate) {
+      notes.push(noteInfo);
+    } else {
+      isUpDate = false;
+      notes[updateId] = noteInfo;
+    }
+
     //saving notes to localstorage
     localStorage.setItem("notes", JSON.stringify(notes));
     closeIcon.click();
+    showNotes();
   }
 });
